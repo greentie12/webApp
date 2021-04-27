0a2dac7e3e1b1b307f58022f3a577e7cebacaefa
greentie12 <tdominguez@fordham.edu>
2021-04-27 14:36:00 -0400

    storage.js refactor

--- js/storage.js
+++ js/storage.js
@@ -5,24 +5,29 @@ const timeZone = document.getElementById("timezone");
 const option = timeZone.querySelector("option");
 const submit = document.getElementById("save");
 
+// function to save current checked boolean value of checkNotify to localstorage
 const saveNotify = () => {
   let checkbox = document.getElementById("checkNotify");
   localStorage.setItem("checkNotify", checkbox.checked);
 };
 
+// function to save current checked boolean value of checkPublic to localstorage
 const savePublic = () => {
   let checkbox = document.getElementById("checkPublic");
   localStorage.setItem("checkPublic", checkbox.checked);
 };
 
+// function to save current timezone.value to localstorage
 const saveTimeZone = () => {
   let timezone = document.getElementById("timezone");
   localStorage.setItem("timezone", timezone.value);
 };
 
+// setting the checked or unchecked value to the input
 document.getElementById("checkNotify").checked = notifyValue;
 document.getElementById("checkPublic").checked = publicValue;
 
+// setting the innerText to the option element
 if (timeZoneValue === null) {
   option.innerText = "Select Timezone";
 } else {
